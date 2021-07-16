f060a959c5c38f0c4939802e073303d91ed2f860
Dave <dave.cadelina@outlook.com>
2021-07-17 01:25:44 +1000

    maeterialize implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
-
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
 const App = () => {
+
+  useEffect(() => {
+    M.AutoInit();
+  })
+
   return (
     <div className="App">
       My App
