0f7606ca0024ae58a6192e44bac4e4f3c8d697e8
Dave <dave.cadelina@outlook.com>
2021-07-17 08:47:51 +1000

    added logs component successfully

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
+import { Fragment, useEffect } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs'
 
 const App = () => {
 
   useEffect(() => {
+    // -- Materialize JS
     M.AutoInit();
   })
 
   return (
-    <div className="App">
-      My App
-    </div>
+      <Fragment>
+        <SearchBar />
+          <div className="container">
+              <Logs />
+          </div>
+      </Fragment>
   );
 }
 
