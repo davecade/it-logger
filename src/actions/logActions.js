ab0e34c75b6fcc98d5783aa5789f489aa485b7a5
Dave <dave.cadelina@outlook.com>
2021-07-19 12:17:37 +1000

    added search Logs action

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG, SET_CURRENT, CLEAR_CURRENT, UPDATE_LOG} from './types'
+import {GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG, SET_CURRENT, CLEAR_CURRENT, UPDATE_LOG, SEARCH_LOGS} from './types'
 
 export const getLogs = () => async dispatch => {
     try {
@@ -106,6 +106,25 @@ export const updateLog = log => async dispatch => {
 
         dispatch({
             type: UPDATE_LOG,
+            payload: data
+        })
+    } catch(error) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: error.response.data
+        })
+    }
+}
+
+export const searchLogs = (text) => async dispatch => {
+    try {
+        setLoading();
+    
+        const res = await fetch(`/logs?q=${text}`)
+        const data = await res.json();
+    
+        dispatch({
+            type: SEARCH_LOGS,
             payload: data
         })
     } catch(error) {
--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, SET_CURRENT, CLEAR_CURRENT, DELETE_LOG, UPDATE_LOG} from '../actions/types'
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, SET_CURRENT, CLEAR_CURRENT, DELETE_LOG, UPDATE_LOG, SEARCH_LOGS} from '../actions/types'
 
 const initialState = {
     logs: [],
@@ -41,6 +41,11 @@ export default (state = initialState, action) => {
             return {
                 ...state,
                 logs: state.logs.map(log => log.id===action.payload.id ? action.payload : log),
+            }
+        case SEARCH_LOGS:
+            return {
+                ...state,
+                logs: action.payload,
             }
         case SET_LOADING:
             return {
